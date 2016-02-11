72b4f818edcf3a6ea69e41a828bac90a61dc5113
Thomas Ersfeld <thomas.ersfeld@gmail.com>
2016-02-11 19:51:20 +0100

    wip

--- src/DeliveryMain.java
+++ src/DeliveryMain.java
@@ -8,16 +8,36 @@ import java.io.IOException;
 public class DeliveryMain {
     public static void main(String argv[]) {
 
-
         System.out.println("Test");
 
-        String filename = "busy_day.in";
+
+        int r = 0;
+        int c = 0;
+        int d = 0;
+        int t = 0;
+        int p = 0;
+
+        String filename = "schlepp.in";
 
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             String line;
-            while ((line = br.readLine()) != null) {
-                // process the line.
-            }
+            String[] split;
+
+            //first line
+            line = br.readLine();
+            split = line.split(" ");
+
+            r = Integer.parseInt(split[0]);
+            c = Integer.parseInt(split[1]);
+            d = Integer.parseInt(split[2]);
+            t = Integer.parseInt(split[3]);
+            p = Integer.parseInt(split[4]);
+
+
+//            while ((line = br.readLine()) != null) {
+//                // process the line.
+//
+//            }
         } catch (IOException e) {
             e.printStackTrace();
         }
