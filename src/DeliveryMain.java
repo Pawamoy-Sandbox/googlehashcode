ef3f54b6efafab439ca4af74800352f710963129
Thomas Ersfeld <thomas.ersfeld@gmail.com>
2016-02-11 19:45:03 +0100

    wip

--- src/DeliveryMain.java
+++ src/DeliveryMain.java
@@ -1,8 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Created by pawantu on 11/02/16.
  */
 public class DeliveryMain {
     public static void main(String argv[]) {
+
+
         System.out.println("Test");
+
+        String filename = "busy_day.in";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                // process the line.
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
